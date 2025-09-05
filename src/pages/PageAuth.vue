89cf08e78a8d2cb36bad9ea2f077e2c4962e587a
Iasmin Cristina Flores de Toledo S. Prado <iasmincristina@gmail.com>
2025-09-05 11:53:59 -0300

    new

--- src/pages/PageAuth.vue
+++ src/pages/PageAuth.vue
@@ -1,13 +1,47 @@
 <template>  
-    <q-page class="q-pa-md">
-        <p>auth page</p>
+    <q-page class="q-pa-xl">
+        <q-card class="auth-tabs q-pa-md" flat bordered>
+        <q-tabs
+          v-model="tab"
+          dense
+          class="text-grey text-justify"
+          active-color="secondary"
+          indicator-color="secondary"
+          narrow-indicator
+        >
+          <q-tab name="login" label="Log-in" />
+          <q-tab name="register" label="Register" />
+        </q-tabs>
+
+        <q-separator />
+
+        <q-tab-panels v-model="tab" animated>
+          <q-tab-panel name="login">
+            <div class="text-h6">Log-in</div>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          </q-tab-panel>
+
+          <q-tab-panel name="register">
+            <Register/>
+          </q-tab-panel>
+        </q-tab-panels>
+      </q-card>
+
     </q-page>
 </template>
 
-<script>
+<script setup>
+import { ref } from 'vue'
+import Register from 'src/components/Auth/Register.vue'
 
+
+    const tab = ref('login');
 </script>
 
 <style>
+.auth-tabs {
+  max-width: 500px;
+  margin: 0 auto;
+}
 
 </style>
